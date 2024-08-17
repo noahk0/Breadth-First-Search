6251a5c77d2de1d40966c85f2461d571fc8b879b
noahk0 <noah.kim@mail.utoronto.ca>
2024-08-18 08:20:18 +0900

    Update !medium Number of Islands O(nm).py

--- !medium Number of Islands O(nm).py
+++ !medium Number of Islands O(nm).py
@@ -2,16 +2,17 @@ def numIslands(self, grid: List[List[str]]) -> int:
     islands, way = 0, ((0, 1), (0, -1), (1, 0), (-1, 0))
 
     def dfs(x, y):
-        if 0 <= x < len(grid) and 0 <= y < len(grid[0]) and grid[x][y] == '1':
-            grid[x][y] = None
+        grid[x][y] = None
 
-            for i, j in way:
+        for i, j in way:
+            if 0 <= x + i < len(grid) and 0 <= y + j < len(grid[0]) and grid[x + i][y + j] == '1':
                 dfs(x + i, y + j)
             
 
     for x in range(len(grid)):
         for y in range(len(grid[0])):
-            islands += grid[x][y] == '1'
-            dfs(x, y)
+            if grid[x][y] == '1':
+                dfs(x, y)
+                islands += 1
                 
     return islands
