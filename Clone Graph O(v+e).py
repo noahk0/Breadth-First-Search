8211ad61a8b5142ac9d4033db7610b5777dd6c9f
noahk0 <noah.kim@mail.utoronto.ca>
2024-09-04 09:24:06 +0900

    Update and rename #medium Clone Graph O(v+e).py to Clone Graph O(v+e).py

---
+++ Clone Graph O(v+e).py
@@ -0,0 +1,17 @@
+def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+    if not node:
+        return
+        
+    q, graph = deque([node]), {node.val: Node(node.val)}
+
+    while q:
+        for neighbor in q[0].neighbors:
+            if neighbor.val not in graph:
+                q.append(neighbor)
+                graph[neighbor.val] = Node(neighbor.val)
+
+            graph[q[0].val].neighbors.append(graph[neighbor.val])
+
+        q.popleft()
+
+    return graph[node.val]
